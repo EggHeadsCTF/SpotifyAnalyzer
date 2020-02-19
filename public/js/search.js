304d4d637dbd66e65b299ccff8595efabf045b5e
Daniel Pan <daniel.panhead1@gmail.com>
2020-02-19 18:30:29 -0500

    Added dropdown for graphs

--- public/js/search.js
+++ public/js/search.js
@@ -1,12 +1,16 @@
 $(document).ready(() => {
 
-    $(".navbar-burger").click(function() {
+    $(".navbar-burger").click(() => {
 
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         $(".navbar-burger").toggleClass("is-active");
         $(".navbar-menu").toggleClass("is-active");
 
     });
+
+    $(".dropdown").click(() => {
+        $(".dropdown").toggleClass("is-active");
+    })
 
     function fadeIn() {
         var element = document.getElementById("loadIn");
